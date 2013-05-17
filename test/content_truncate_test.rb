481a0393b17f947674a504e79f44a043c48434e3
soffolk <lianxin.zhu@easyread.ly>
2013-05-18 00:11:46 +0800

    add test to keep method work

---
+++ test/content_truncate_test.rb
@@ -0,0 +1,21 @@
+require 'test/unit'
+require_relative '../lib/content_truncate'
+
+class ContentTruncateTest < Test::Unit::TestCase
+  
+  def mock_string
+    "Truncate a string down to \n x characters. <br/> Beware of truncating text that has HTML elements in it."
+  end
+  
+  def test_content_truncate_for_no_separator
+    assert_equal "Truncate a string...", mock_string.content_truncate(20)
+  end
+  
+  def test_content_truncate_for_one_separator
+    assert_equal "Truncate a string down to \n x characters.", mock_string.content_truncate(100, "<br/>")
+  end
+  
+  def test_content_truncate_for_separators
+    assert_equal "Truncate a string", mock_string.content_truncate(20, "<br/>", "\n", " ")
+  end
+end
