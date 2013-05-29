ba28ecc8af32794d38e85717b9f471cd0d88cf55
soffolk <lianxin.zhu@easyread.ly>
2013-05-29 14:48:48 +0800

    fix bug when process sep priority

--- lib/content_truncate.rb
+++ lib/content_truncate.rb
@@ -4,21 +4,26 @@ module ContentTruncate
   module String
     def content_truncate limit_length, *separators
       sub_string = self.dup
-      sep = separators.pop
+      sep = separators.shift
       if sep
-        while sep
-          index = sub_string.index(sep)
-          if index <= limit_length
-            while index && index <= limit_length
-              prev_index, index = index, sub_string.index(sep, index+1)
-            end
-            return sub_string[0...prev_index].strip
-          else
-            sep = separators.pop
-          end
-        end
+        sub_string.send(:smart_truncate, limit_length, sep, separators)
       else
         sub_string.truncate(limit_length, separator: sep)
+      end
+    end
+    
+    private
+    def smart_truncate limit_length, sep, *separators
+      while sep
+        index = self.index(sep)
+        if index <= limit_length
+          while index && index <= limit_length
+            prev_index, index = index, self.index(sep, index+1)
+          end
+          return self[0...prev_index].strip
+        else
+          sep = separators.shift
+        end
       end
     end
   end
--- test/content_truncate_test.rb
+++ test/content_truncate_test.rb
@@ -4,7 +4,7 @@ require_relative '../lib/content_truncate'
 class ContentTruncateTest < Test::Unit::TestCase
   
   def mock_string
-    "Truncate a string down to \n x characters. <br/> Beware of truncating text that has HTML elements in it."
+    "Truncate a string down to \n x characters. <br/> Beware of truncating \n text that has HTML elements in it."
   end
   
   def test_content_truncate_for_no_separator
@@ -16,6 +16,6 @@ class ContentTruncateTest < Test::Unit::TestCase
   end
   
   def test_content_truncate_for_separators
-    assert_equal "Truncate a string", mock_string.content_truncate(20, "<br/>", "\n", " ")
+    assert_equal "Truncate a string down to", mock_string.content_truncate(60, "\n", "<br/>", " ")
   end
 end
